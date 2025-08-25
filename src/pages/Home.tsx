498561e1c13a6475ae82d06b7951ccece6b00997
Aminulhoque01 <aminuhoque53@gmail.com>
2025-08-26 02:10:32 +0600

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,46 @@
+import { Button } from '@/components/ui/button';
+import banner from '@/assets/images/banner.gif';
+import hero from '@/assets/images/hero.png';
+import { Link } from 'react-router-dom';
+import Footer from '@/layouts/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <div className="md:flex justify-between items-center h-[calc(100vh-80px)] max-w-7xl mx-auto ">
+        <div>
+          <h1 className="text-6xl font-black text-primary mb-2">
+            WelCome to Our <br /> E-commers Store
+          </h1>
+          <p className="text-secondary font-semibold text-xl">
+            choice your best product from here
+          </p>
+          <div className="text-primary mt-20">
+            <p>Bluetooth 5.2 for easy, secure communication</p>
+            <p>Precise 143 Amoled display for clear visuals</p>
+          </div>
+          <Button className="mt-5">Learn more</Button>
+        </div>
+        <div className="relative -right-14 ">
+          <div className='w-[500px] h-[700px]  flex justify-center items-center'>
+           <img src={banner} alt="" className="w-full h-auto object-contain" style={{ filter: 'none', opacity: 1 }} />
+          </div>
+        </div>
+      </div>
+      <div className="mb-96">
+        <div>
+          <img className="mx-auto" src={hero} alt="" />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-5xl font-black text-primary uppercase mt-10">
+            The future of tech is here
+          </h1>
+          <Button className="mt-10" asChild>
+            <Link to="/products">Brows all products</Link>
+          </Button>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
